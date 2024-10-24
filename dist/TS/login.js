b7f2bc22bb339673e6621e9e1a69dd80cdde4604
mohammedminhajcs <mohammedminhaj901@gmail.com>
2024-10-24 10:13:28 +0530

    login modification

---
+++ dist/TS/login.js
@@ -0,0 +1,15 @@
+"use strict";
+function togglePasswordVisibility() {
+    const passwordField = document.getElementById('password');
+    const toggleIcon = document.getElementById('togglePassword');
+    if (passwordField && toggleIcon) {
+        if (passwordField.type === 'password') {
+            passwordField.type = 'text';
+            toggleIcon.src = '../../assets/login/Mask group.svg'; // Change to open eye icon
+        }
+        else {
+            passwordField.type = 'password';
+            toggleIcon.src = '../../assets/login/el_eye-close.svg'; // Change back to closed eye icon
+        }
+    }
+}
