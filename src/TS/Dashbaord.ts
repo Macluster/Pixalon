9caeed19e5b36d7ca4950325c39af9ce653e9156
Macluster <deepakjudejohn@gmail.com>
2024-10-25 11:31:43 +0530

    search functionality added

--- src/TS/Dashbaord.ts
+++ src/TS/Dashbaord.ts
@@ -86,6 +86,42 @@ function onPosterCreate() {
     window.location.href=`workspace.html?name=title&height=600&width=600`
 }
 
+function onSearched(event:Event)
+{
+    console.log("hai")
+    const menuDiv = document.getElementById("menu");
+    const div = document.createElement("div")
+    div.style.display = "flex"
+    div.style.flexDirection = "column"
+    div.style.alignItems = "center";
+    div.style.width = "100%"
+    div.style.height = "100%"
+    menuDiv!.innerHTML = "<div onclick='onSavedItemsbackButtonPressed()' style='width:100%;display:flex;align-items:center;margin-top:20px;gap:10px'><a href='../Pages/dashboard.html'><i style='display:flex;align-items:center;height:20px;' class='fa-solid fa-arrow-left'></i></a><h3 >Searched Items<h3></div>";
+    menuDiv!.appendChild(div)
+    menuDiv!.style.height = "100%";
+
+
+    savedItems.map((e) => {
+
+        
+        let ele=event!.target! as HTMLInputElement
+        if(e.fileName.startsWith(ele.value))
+        {
+            let card = `
+            <div class="savedItem" >
+            <img src='${e.image}' style="height:20px;width:20px;margin-top:0px" />   
+            <h5 style="margin:0px;color:white">${e.fileName}</h5>
+            
+            </div>
+            `
+            div!.innerHTML = div!.innerHTML + card;
+        }
+
+       
+    })
+
+}
+
 
 
 
