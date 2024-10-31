9e5a6092a3915a45adedc539da28d1a1ccc633ee
mohammedminhajcs <mohammedminhaj901@gmail.com>
2024-10-31 21:12:10 +0530

    fix: index-add cookie

--- src/TS/Backend/login.js
+++ src/TS/Backend/login.js
@@ -28,6 +28,7 @@ function login() {
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
             // Signed in successfully
+            setCookie("loggedIn", "true", 1);
             const user = userCredential.user;
             // Redirect to dashboard
             window.location.href = 'dashboard.html';
@@ -63,4 +64,24 @@ document.addEventListener('keydown', (event) => {
 function isValidEmail(email) {
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailRegex.test(email);
+}
+
+//cookie
+function setCookie(name, value, days) {
+    const d = new Date();
+    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000)); // Expiration in days
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = `${name}=${value};${expires};path=/`;
+}
+
+// Utility function to get a cookie by name
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
 }
